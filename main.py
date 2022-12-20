4c7010d5ad43d9272eb865306988990d95cd592e
mktechnologies <>
2022-12-20 22:20:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Bob")
+print("September 20th 2029")
+print("I am signing up for Replit's 100 days of Python challenge!")
+print("""I will make sure to spend time tvery day coding
+along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do
+this from my device whereever I happen to be. No excuses
+for not coding from the middle of a field!""")
+print("I am feeling \U0001F973")
+print("You can follow my progress at replit.com/@mktechnologies")
